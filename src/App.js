7e3e50bc63cbbf1646f0805a6835245dc5b33efc
Marcos676 <patino-marcos@outlook.com>
2025-05-30 20:19:36 -0300

    sube estructura básica + estructura JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Burguer button icon</div>
       </header>
+      <main>
+        <div className="title-description">
+          <h1>Título</h1>
+          <p>Descripción de uso</p>
+        </div>
+        <div className="functional-app">
+          <div className="inputs-box">
+            <input
+              className="original-price"
+              type="number"
+              placeholder="Precio original"
+            />
+            <input
+              className="porcent-to-discount"
+              type="number"
+              placeholder="Porcentaje de descuento"
+            />
+            %
+          </div>
+          <div className="result-box">
+            <p className="discount">200</p>
+            <p className="final-price">1050</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
