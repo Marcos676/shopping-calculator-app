df5198599ebb4d38eebf46f5df39478277174017
Marcos676 <patino-marcos@outlook.com>
2026-02-02 23:01:56 -0300

    Modificar respuesta de creación de tickets para simplificar el estado de la respuesta y ajustar mensajes de validación

--- src/validations/createTicketValidator.js
+++ src/validations/createTicketValidator.js
@@ -4,11 +4,11 @@ const validations = [
   check("name")
     .trim()
     .notEmpty()
-    .withMessage("Se requiere un nombre para la lista"),
+    .withMessage("Se requiere un nombre"),
   body("name")
     .custom((value) => value.trim().length < 31)
-    .withMessage("Solo puede tener hasta 30 caracteres"), //Menos de 30 caracteres
-  check("productList").notEmpty().withMessage("La lista no puede estar vacía"),
+    .withMessage("Debe tener hasta 30 caracteres"), //Menos de 30 caracteres
+  body("productList").custom((value) => value.length !== 0).withMessage("No se puede guardar un carrito vacío"),
 ];
 
 export default validations;
