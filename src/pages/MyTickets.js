49105802fa503a8ce2b4fea5f90d5e7ffe9746ea
Marcos676 <patino-marcos@outlook.com>
2026-02-05 19:37:04 -0300

    Añade la librería date-fns para el manejo de fechas; implementa la página "Mis Tickets" con la obtención y formato de fechas de creación de tickets; crea estilos para la lista de tickets.

--- src/pages/MyTickets.js
+++ src/pages/MyTickets.js
@@ -1,12 +1,65 @@
-export const MyTickets = ({}) => {
+import "../styles/MyTickets.css";
+import { parseISO, format } from 'date-fns';
+import { useState, useEffect } from "react";
+
+
+export const MyTickets = () => {
+ const [tickets, setTickets] = useState([]);
+
+    const getTickets = async () => {
+        try {
+            const response = await fetch(process.env.REACT_APP_API_URL + "ticket", {
+          method: "GET",
+          credentials: "include",
+        });
+        switch (response.status) {
+            case 200:
+                let tickets = await response.json()
+                // parcea la fecha de creacion de cada ticket
+                tickets.forEach(ticket => {
+                    ticket.created_at = format(parseISO(ticket.created_at), 'dd/MM/yyyy')
+                });
+                setTickets(tickets)
+            break;
+        
+            default:
+                break;
+        }
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    useEffect(()=> {
+getTickets()
+    }, [])
+
+
   return (
     <>
       <div className="title-description">
         <h2>Mis Tickets</h2>
       </div>
-      <div>
+      <ul className="ticket-list" >
+        {tickets.map(({id, name, products_list, created_at}) => {
+            return (<li className={`item-list item-id-${id}`} key={id}><div>{name}</div><div>{created_at}</div></li>)
+        })}
         
-      </div>
+{/*         <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+        <li className="item-list"><div>Coto</div><div>1/02/2026</div></li>
+         */}
+      </ul>
     </>
   );
 };
