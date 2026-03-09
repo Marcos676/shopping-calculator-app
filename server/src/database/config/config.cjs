7f9457bfe4eb756301e727851f6518808e02b624
Marcos676 <patino-marcos@outlook.com>
2026-03-09 19:27:36 -0300

    reaaliza cambio de extension a archivo .sequelizerc y en el script de sequelize para que puedan ejecutarse las configuraciones de la base de datos en el contenedor

--- server/src/database/config/config.cjs
+++ server/src/database/config/config.cjs
@@ -7,20 +7,21 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql",
-    //"logging": false // Oculta las querys impresas por consola. Util en producción
+    
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
+    //"logging": false // Oculta las querys impresas por consola. Util en producción
   }
 }
